fb4e3dd8cb58f74967739607df71aa7658ee1167
Brightcells <brightcells@gmail.com>
2016-08-15 15:33:16 +0800

    Support FileHandler/RotatingFileHandler/TimedRotatingFileHandler

--- django_rlog/management/commands/rlog.py
+++ django_rlog/management/commands/rlog.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import logging.handlers
 
 from django.conf import settings
 from django_six import CompatibilityBaseCommand
@@ -11,6 +12,10 @@ r = settings.REDIS_CACHE
 
 DEFAULT_CHANNEL = 'django-logit'
 DEFAULT_FILENAME = '/tmp/django-logit.log'
+DEFAULT_HANDLER = 'TimedRotatingFileHandler'
+DEFAULT_WHEN = 'midnight'
+DEFAULT_MAX_BYTES = 15728640,  # 1024 * 1024 * 15B = 15MB
+DEFAULT_BACKUP_COUNT = 10
 
 
 class Command(CompatibilityBaseCommand):
@@ -29,11 +34,52 @@ class Command(CompatibilityBaseCommand):
             default=DEFAULT_FILENAME,
             help='Filename log write into.',
         )
+        parser.add_argument(
+            '--handler',
+            dest='handler',
+            default='FileHandler',
+            help='FileHandler to use.',
+        )
+        parser.add_argument(
+            '--when',
+            dest='when',
+            default=DEFAULT_WHEN,
+            help='When of TimedRotatingFileHandler.',
+        )
+        parser.add_argument(
+            '--maxBytes',
+            dest='maxBytes',
+            default=DEFAULT_MAX_BYTES,
+            type=int,
+            help='MaxBytes of RotatingFileHandler.',
+        )
+        parser.add_argument(
+            '--backupCount',
+            dest='backupCount',
+            default=DEFAULT_BACKUP_COUNT,
+            type=int,
+            help='BackupCount of TimedRotatingFileHandler/RotatingFileHandler.',
+        )
+
+    def getHandler(self, **options):
+        """Support FileHandler/RotatingFileHandler/TimedRotatingFileHandler"""
+        filename = options.get('filename', DEFAULT_FILENAME)
+        handler = options.get('handler', DEFAULT_FILENAME)
+        when = options.get('when', DEFAULT_WHEN)
+        maxBytes = options.get('maxBytes', DEFAULT_MAX_BYTES)
+        backupCount = options.get('backupCount', DEFAULT_BACKUP_COUNT)
+
+        if handler == 'TimedRotatingFileHandler':
+            return logging.handlers.TimedRotatingFileHandler(filename, when=when, backupCount=backupCount)
+        elif handler == 'RotatingFileHandler':
+            return logging.handlers.RotatingFileHandler(filename, maxBytes=maxBytes, backupCount=backupCount)
+        else:
+            return logging.FileHandler(filename)
 
     def handle(self, *args, **options):
         logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
-        logger.addHandler(logging.FileHandler(options.get('filename', DEFAULT_FILENAME)))
+        logger.addHandler(self.getHandler(**options))
         p = r.pubsub()
         p.subscribe(options.get('channel', DEFAULT_CHANNEL))
         for item in p.listen():
